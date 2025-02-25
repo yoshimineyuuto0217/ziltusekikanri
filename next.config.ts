b746af45ad65c3dd009e0b07dac6d81bfffbfad2
吉嶺勇斗 <jilingyongdou@gmail.com>
2025-02-26 03:05:16 +0900

    ミドルウェアを適用

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 import type { Configuration } from 'webpack';
 
 const nextConfig: NextConfig = {
+  env: {
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET, // 環境変数を Next.js に明示的に渡す
+  },
   webpack: (config: Configuration, { isServer }: { isServer: boolean }) => {
     if (!isServer) {
       config.resolve = config.resolve || {};
