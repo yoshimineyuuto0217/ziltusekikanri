6d1b103443abe4e6e22056f625e26ec75a0b51eb
吉嶺勇斗 <jilingyongdou@gmail.com>
2025-03-14 18:00:12 +0900

    変更

--- middleware.ts
+++ middleware.ts
@@ -23,4 +23,5 @@ export async function middleware(req: NextRequest) {
 
 export const config = {
     matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|login).*)'],
+    runtime: 'nodejs', 
 };
